721c504d857de45094a2410261692c666f8ad9a4
Aidan Hurley-Kalici <57423704+Aidanjhk@users.noreply.github.com>
2020-04-17 13:24:18 -0400

    Update Random_Number_GEN.cpp

--- Project1/Project1/Random_Number_GEN.cpp
+++ Project1/Project1/Random_Number_GEN.cpp
@@ -5,6 +5,22 @@ using namespace std;
 
 int main()
 {
+	int NUM2;
 	int NUM = ((rand() % 100) + 1);
-	cout << NUM << endl;
+	cout << "Enter Number" << endl;
+	cin >> NUM2;
+	
+	if (NUM == NUM2)
+	{
+		cout << "Correct" << endl;
+	}
+	else if (NUM > NUM2)
+	{
+		cout << "To Low" << endl;
+	}
+	else if (NUM < NUM2)
+	{
+		cout << "To High" << endl;
+	}
+	return 0;
 }
